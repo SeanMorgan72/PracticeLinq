ba9a7dbed38352d47b009e9926c6fd16603f2a60
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-09-08 11:43:27 -0500

    Linq to game array

---
+++ PracticeLinq/GameNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PracticeLinq
+{
+    public class GameNames
+    {
+        public GameNames()
+        {
+        }
+
+        public GameNames(string games, string[] gameNames)
+        {
+            Games = games;
+            this.gameNames = gameNames;
+        }
+
+        public string Games { get; set; }
+
+               
+        public string[] gameNames = new string[] { "Mortal Kombat", "Soul Caliber", "D.O.A", "Halo", "Ninja Gaiden" };
+        
+    }
+}
---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new GameNames();
+
+            var show = games.gameNames.OrderBy(k => k.Length);
+
+            foreach(var name in show)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
